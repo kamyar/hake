a10b699da2defe3bc0badd838c87b0143f16a3ca
Engin Deniz Usta <edusta@Engins-MacBook-Pro.local>
2016-02-07 20:22:35 +0200

    Initial Commit

--- classDef.py
+++ classDef.py
@@ -70,6 +70,8 @@ class Matrix():
 			if self.percentageOfBlackInSquare(i, j, squareRadius) > 0.9:
 				if squareRadius > bestFoundRadius:
 					foundRow, foundColumn, bestFoundRadius = i, j, squareRadius
+			else:
+				break
 
 		if bestFoundRadius != -1:
 			print "FOUND ", foundRow, foundColumn, bestFoundRadius
